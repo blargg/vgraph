45bc2357583ca6b731a178d927ed533ac45374e7
blargg <tomjankauski@gmail.com>
2022-08-13 00:11:43 -0700

    Add path length function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
+use num::traits::Zero;
 use std::collections::{HashMap, VecDeque};
 use std::hash::Hash;
+use std::ops::AddAssign;
 
 /// Virtual Graph.
 pub trait VGraph {
     type Node;
 
+    // Measure of the distance between nodes
+    type Dist;
+
     fn all_nodes() -> Vec<Self::Node>;
     fn out_edges(node: Self::Node) -> Vec<Self::Node>;
+
+    fn dist(from: Self::Node, to: Self::Node) -> Self::Dist;
 }
 
 pub fn breadth_first_search<G>(start: G::Node, end: G::Node) -> Option<Vec<G::Node>>
@@ -23,7 +30,6 @@ where
             return Some(back_track(&prev, end));
         }
 
-
         for next in G::out_edges(cur) {
             // Only insert nodes we have not explored yet
             if !prev.contains_key(&next) {
@@ -34,6 +40,24 @@ where
     }
 
     None
+}
+
+pub fn path_length<G>(path: Vec<G::Node>) -> G::Dist
+where
+    G: VGraph,
+    G::Node: Copy,
+    G::Dist: Copy + AddAssign + Zero,
+{
+    let mut distance = G::Dist::zero();
+    for window in path.windows(2) {
+        if let &[from, to] = window {
+            distance += G::dist(from, to);
+        } else {
+            panic!("Windows were not full.");
+        }
+    }
+
+    distance
 }
 
 /// Given a prev map, where each index points to the previous value.
@@ -61,7 +85,7 @@ mod tests {
         type Node = usize;
 
         fn all_nodes() -> Vec<Self::Node> {
-            vec![1,2,3]
+            vec![1, 2, 3]
         }
 
         fn out_edges(node: Self::Node) -> Vec<Self::Node> {
@@ -72,10 +96,22 @@ mod tests {
                 _ => vec![],
             }
         }
+
+        type Dist = i32;
+
+        // If a node is in the graph, then the distance is 1
+        fn dist(_: Self::Node, _: Self::Node) -> Self::Dist {
+            1
+        }
     }
 
     #[test]
     fn breadth_first_search_works() {
-        assert_eq!(Some(vec![1,2,3]), breadth_first_search::<Ex>(1,3));
+        assert_eq!(Some(vec![1, 2, 3]), breadth_first_search::<Ex>(1, 3));
+    }
+
+    #[test]
+    fn path_length_works() {
+        assert_eq!(2, path_length::<Ex>(vec![1, 2, 3]))
     }
 }
